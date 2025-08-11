e2dd2e29c8f7801102762e7674490ad20f45e3ab
Indranil Halder <indranilhalder.dev@gmail.com>
2025-08-11 15:06:09 +0530

    shadcn ui setup
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
-const HomePage = () => {
-  return <div>HomePage</div>;
+import { FC } from "react";
+import { Button } from "@/components/ui/button";
+
+const HomePage: FC = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <Button>Click me</Button>
+    </div>
+  );
 };
 
 export default HomePage;
