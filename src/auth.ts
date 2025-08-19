cd7c6d4fdb76dcc302250bdcb90c322652ce1c8b
Indranil Halder <indranilhalder.dev@gmail.com>
2025-08-19 21:42:18 +0530

    Database and drizzle setup
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,9 @@
 import NextAuth from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import { db } from "./db/drizzle";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [GitHubProvider],
+  adapter: DrizzleAdapter(db),
 });
