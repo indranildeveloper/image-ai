75b24c522b2658ed7925ec1ace61bbded63e814c
Indranil Halder <indranilhalder.dev@gmail.com>
2025-08-20 12:55:56 +0530

    Google provider login
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

--- src/auth.ts
+++ src/auth.ts
@@ -1,10 +1,11 @@
 import NextAuth from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import { db } from "./db/drizzle";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [GitHubProvider],
+  providers: [GitHubProvider, GoogleProvider],
   adapter: DrizzleAdapter(db),
   pages: {
     signIn: "/login",
