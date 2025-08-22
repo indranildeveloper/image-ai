5a914e44328e9a3f838fa151b2f14cd38437e714
Indranil Halder <indranilhalder.dev@gmail.com>
2025-08-22 22:18:34 +0530

    Project update mutation
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

---
+++ src/features/projects/api/useUpdateProject.ts
@@ -0,0 +1,47 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { InferRequestType, InferResponseType } from "hono";
+import { toast } from "sonner";
+import { client } from "@/hono/hono";
+
+type ResponseType = InferResponseType<
+  (typeof client.api.projects)[":projectId"]["$patch"],
+  200
+>;
+type RequestType = InferRequestType<
+  (typeof client.api.projects)[":projectId"]["$patch"]
+>["json"];
+
+export const useUpdateProject = (projectId: string) => {
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation<ResponseType, Error, RequestType>({
+    mutationFn: async (json) => {
+      const response = await client.api.projects[":projectId"].$patch({
+        json,
+        param: { projectId },
+      });
+
+      if (!response.ok) {
+        throw new Error("Something went wrong while updating the project!");
+      }
+
+      return await response.json();
+    },
+    onSuccess: () => {
+      // TODO: invalidate projects query
+      queryClient
+        .invalidateQueries({ queryKey: ["project", { projectId }] })
+        .catch((error) => {
+          console.error(
+            "Something went wrong while invalidating project update query!",
+            error,
+          );
+        });
+    },
+    onError: () => {
+      toast.error("Something went wrong while updating the project!");
+    },
+  });
+
+  return mutation;
+};
